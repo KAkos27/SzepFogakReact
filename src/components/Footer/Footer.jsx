721eec9612de065a4f248c2fb5e2844c218e6af0
KAkos27 <akoskosztolanyi@gmail.com>
2024-09-05 12:05:47 +0200

    footer kesz

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,7 +1,23 @@
+import logo from "../../../public/logo.jpg";
 import "./Footer.css";
 
 const Footer = () => {
-  return <div className="footer">Footer</div>;
+  const emailLink =
+    "https://mail.google.com/mail/u/0/?tab=rm&ogbl#inbox?compose=GTvVlcRzDCwcnLsnbVNPTjGhthQDPNjlxZbXxZlGlPjkdRRMQttwLVCwGtmpSCgcstbNNvvNWGMJH";
+
+  return (
+    <div className="footer">
+      <img src={logo} alt="szÉPfogak" />
+      <div className="footer__info">
+        <h2>Magyar Ildikó</h2>
+        <p>Fogászati Klinikai higiénikus</p>
+        <p>06-30/355-8913</p>
+        <a href={emailLink}>hun.ildiko@gmail.com</a>
+        <p>Érd Szabadság tér 9</p>
+        <small>(legfelső emelet)</small>
+      </div>
+    </div>
+  );
 };
 
 export default Footer;
