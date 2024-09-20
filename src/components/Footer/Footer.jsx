ac8e9d78addda715fed959ca52ec7643a17cd08a
kosztolanyia <kosztolanyia@wildom.com>
2024-09-20 08:25:24 +0200

    hupszi

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,4 +1,4 @@
-import logo from "../../../public/logo.jpg";
+import logo from "/logo.jpg";
 import "./Footer.css";
 
 const Footer = () => {
--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -18,25 +18,24 @@ const Nav = () => {
         className="icon menu-button"
         onClick={() => handleModalState(true)}
       ></span>
-      <div className="wrapper" onClick={() => handleModalState(false)}>
-        <Modal
-          className="nav"
-          open={open}
-          onEscape={() => handleModalState(false)}
-        >
-          <span
-            className="icon close-button"
-            onClick={() => handleModalState(false)}
-          ></span>
-          <div className="link-container">
-            {routes.map((route) => (
-              <Link key={route.id} to={route.path}>
-                {route.title}
-              </Link>
-            ))}
-          </div>
-        </Modal>
-      </div>
+
+      <Modal
+        className="nav"
+        open={open}
+        onEscape={() => handleModalState(false)}
+      >
+        <span
+          className="icon close-button"
+          onClick={() => handleModalState(false)}
+        ></span>
+        <div className="link-container">
+          {routes.map((route) => (
+            <Link key={route.id} to={route.path}>
+              {route.title}
+            </Link>
+          ))}
+        </div>
+      </Modal>
     </>
   );
 };
